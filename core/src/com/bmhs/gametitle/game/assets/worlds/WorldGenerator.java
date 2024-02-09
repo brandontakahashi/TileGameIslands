3bed69c42ce4b08f212b695375c860280bf79940
brandontakahashi <brandontakahashi0@gmail.com>
2024-02-08 20:16:54 -0800

    commit

--- core/src/com/bmhs/gametitle/game/assets/worlds/WorldGenerator.java
+++ core/src/com/bmhs/gametitle/game/assets/worlds/WorldGenerator.java
@@ -8,9 +8,7 @@ import com.badlogic.gdx.math.Vector2;
 import com.bmhs.gametitle.gfx.assets.tiles.statictiles.WorldTile;
 import com.bmhs.gametitle.gfx.utils.TileHandler;
 
-
 public class WorldGenerator {
-
     private int worldMapRows, worldMapColumns;
 
     private int seedColor;
@@ -22,54 +20,64 @@ public class WorldGenerator {
         this.worldMapColumns = worldMapColumns;
 
         worldIntMap = new int[worldMapRows][worldMapColumns];
-        seedColor = 2;
+        seedColor = 67;
 
         water();
 
-        Vector2 mapSeed = new Vector2(10, 20);
+//        Vector2 mapSeed = new Vector2(10, 20);
+//
+//        worldIntMap[(int)mapSeed.y][(int)mapSeed.x] = seedColor;
+//
+//        for(int r = 0; r < worldIntMap.length; r++) {
+//            for(int c = 0; c < worldIntMap[r].length; c++){
+//                Vector2 tempVector = new Vector2(c, r);
+//                if(tempVector.dst(mapSeed) < 6){
+//                    worldIntMap[r][c] = 2;
+//                }
+//            }
+//        }
+
+        Vector2 mapSeed = new Vector2(23, 15);
+        int horizontalRadius = 10; // Horizontal radius of the oval
+        int verticalRadius = 6;   // Vertical radius of the oval
 
         worldIntMap[(int)mapSeed.y][(int)mapSeed.x] = seedColor;
 
         for(int r = 0; r < worldIntMap.length; r++) {
             for(int c = 0; c < worldIntMap[r].length; c++){
                 Vector2 tempVector = new Vector2(c, r);
-                if(tempVector.dst(mapSeed) < 6){
-                    worldIntMap[r][c] = 2;
+                double distance = Math.pow((c - mapSeed.x) / horizontalRadius, 2) + Math.pow((r - mapSeed.y) / verticalRadius, 2);
+                if(distance <= 1){
+                    worldIntMap[r][c] = 67;
                 }
             }
         }
+
 
         //seedIslands(1);
 
         //sand
-        searchAndExpand(5, seedColor, 45, 0.99);
-        searchAndExpand(4, seedColor, 44,0.99);
+        searchAndExpand(5, seedColor, 44, 0.99);
+        searchAndExpand(4, seedColor, 45,0.99);
+        searchAndExpand(4, seedColor, 43, 0.7);
         //grass
-        searchAndExpand(2, seedColor, 67, 0.85);
-        searchAndExpand(2, seedColor, 72, 0.3);
+        searchAndExpand(2, seedColor, 72, 0.5);
         searchAndExpand(2, seedColor, 61, 0.3);
 
-        /*
-        Vector2 mapSeed = new Vector2(MathUtils.random(worldIntMap[0].length), MathUtils.random(worldIntMap.length));
-        System.out.println(mapSeed.y + " " + mapSeed.x);
 
-        worldIntMap[(int)mapSeed.y][(int)mapSeed.x] = 4;
-
-        for(int r = 0; r < worldIntMap.length; r++) {
-            for(int c = 0; c < worldIntMap[r].length; c++){
-                Vector2 tempVector = new Vector2(c, r);
-                if(tempVector.dst(mapSeed) < 3){
-                    worldIntMap[r][c] = 2;
-                }
-            }
-        }
-
-         */
-
-
-
-
-
+//        Vector2 mapSeed = new Vector2(MathUtils.random(worldIntMap[0].length), MathUtils.random(worldIntMap.length));
+//        System.out.println(mapSeed.y + " " + mapSeed.x);
+//
+//        worldIntMap[(int)mapSeed.y][(int)mapSeed.x] = 4;
+//
+//        for(int r = 0; r < worldIntMap.length; r++) {
+//            for(int c = 0; c < worldIntMap[r].length; c++){
+//                Vector2 tempVector = new Vector2(c, r);
+//                if(tempVector.dst(mapSeed) < 3){
+//                    worldIntMap[r][c] = 2;
+//                }
+//            }
+//        }
 
         //call methods to build 2D array
         //randomize();
@@ -78,6 +86,7 @@ public class WorldGenerator {
 
         Gdx.app.error("WorldGenerator", "WorldGenerator(WorldTile[][][])");
     }
+
 
     public void water() {
         for(int r = 0; r < worldIntMap.length; r++) {
